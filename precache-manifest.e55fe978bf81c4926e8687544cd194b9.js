0f3d6e8033f0d795f1e7ecd5f26cf872306e2e28
saigal15 <46360205+saigal15@users.noreply.github.com>
2019-05-10 10:11:13 +0200

    Updates

---
+++ precache-manifest.e55fe978bf81c4926e8687544cd194b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4beca5672d3e16c3b3105659eb86052d",
+    "url": "/quizpsycho/index.html"
+  },
+  {
+    "revision": "8a4384dc002caa86e1aa",
+    "url": "/quizpsycho/static/css/main.041b65fe.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/quizpsycho/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "8a4384dc002caa86e1aa",
+    "url": "/quizpsycho/static/js/main.7de1667a.chunk.js"
+  },
+  {
+    "revision": "d9c2e8904503b30f7b26",
+    "url": "/quizpsycho/static/js/runtime~main.de1c7491.js"
+  }
+]);
